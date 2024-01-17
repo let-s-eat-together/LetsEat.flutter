c0b01106746d1b29247577dc255eb3562d020be4
SongJungHyun <201802112@o.cnu.ac.kr>
2024-01-17 14:41:51 +0900

    refactor: main.dart

--- lets_eat/lib/main.dart
+++ lets_eat/lib/main.dart
@@ -4,14 +4,26 @@ import 'package:lets_eat/screen/signup.dart';
 import 'package:lets_eat/screen/test_planlist_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       initialRoute: '/',
+      title: 'Let`s Eat',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       routes: {
         '/': (context) => Login(),
         '/signup' : (context) => Signup(),
         '/planlist' : (context) => Test_Planlist_Screen(), // Planlist_Screen으로 교체
       },
-    ),
-  );
+    );
+  }
 }
