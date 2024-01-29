363bf37ee7511fb9fe26548bf1931718c31bd888
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-01-29 21:53:02 +0900

    check conflict & enable execution

--- lets_eat/lib/screen/home/home_screen.dart
+++ lets_eat/lib/screen/home/home_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lets_eat/const/tabs.dart';
 import 'package:lets_eat/screen/friend/friend_list_screen.dart';
 import 'package:lets_eat/screen/make_plan/make_plan_screen.dart';
+import 'package:lets_eat/screen/message/message_screen.dart';
 import 'package:lets_eat/screen/plan/plan_screen.dart';
+import 'package:lets_eat/screen/profile/profile_screen.dart';
 
 class HomeUI extends StatefulWidget {
   @override
@@ -20,47 +22,55 @@ class _HomeUI extends State<HomeUI> {
 
     MakePlanView(), // Make Plan
 
-    Text(
-      'Alarm',
-      style: TextStyle(fontSize: 30),
-    ), // Alarm
+    MessageScreen(), // Message
 
-    Text(
-      'Profile',
-      style: TextStyle(fontSize: 30),
-    ), // Profile
+    ProfileScreen(), // Profile
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        // BottomNavigationBar Widget
-        type: BottomNavigationBarType.fixed,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text(
+            TABS[_selectedIndex].label,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          // BottomNavigationBar Widget
+          type: BottomNavigationBarType.fixed,
 
-        // BottomNavigation Colors
-        backgroundColor: Color.fromARGB(255, 187, 157, 211),
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white.withOpacity(.60),
+          // BottomNavigation Colors
+          backgroundColor: Color.fromARGB(255, 187, 157, 211),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withOpacity(.60),
 
-        // Selected Index
-        currentIndex: _selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
+          // Selected Index
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
 
-        // BottomNavigation Items
-        items: TABS.map((TabInfo tab) {
-          return BottomNavigationBarItem(
-            icon: Icon(tab.icon),
-            label: tab.label,
-          );
-        }).toList(),
+          // BottomNavigation Items
+          items: TABS.map((TabInfo tab) {
+            return BottomNavigationBarItem(
+              icon: Icon(tab.icon),
+              label: tab.label,
+            );
+          }).toList(),
+        ),
       ),
     );
   }
