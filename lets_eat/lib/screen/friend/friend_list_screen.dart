6191ea03a41d9f74ec86ba8fc9fd0a67b521191c
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-01-29 21:47:50 +0900

    feat: add addFriend api

--- lets_eat/lib/screen/friend/friend_list_screen.dart
+++ lets_eat/lib/screen/friend/friend_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:lets_eat/model/friend.dart';
+import 'package:lets_eat/screen/friend/add_button.dart';
 
 class FriendListScreen extends StatefulWidget {
   @override
@@ -6,44 +9,70 @@ class FriendListScreen extends StatefulWidget {
 }
 
 class _FriendListScreenState extends State<FriendListScreen> {
-  String searchText = "";
+  String searchText = '';
   final List<String> friendList = [
-    "김서근", "포뇨", "하울", "소피아", "캐시퍼", "소스케",
-    "치히로", "하쿠", "가오나시", "제니바", "카브", "마르클",
-    "토토로", "사츠키", "지브리", "스튜디오", "캐릭터"
+    "김서근",
+    "포뇨",
+    "하울",
+    "소피아",
+    "캐시퍼",
+    "소스케",
+    "치히로",
+    "하쿠",
+    "가오나시",
+    "제니바",
+    "카브",
+    "마르클",
+    "토토로",
+    "사츠키",
+    "지브리",
+    "스튜디오",
+    "캐릭터"
   ];
+
+  List<Friend> parseFriends(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Friend>((json) => Friend.fromJson(json)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-      child: Column(
-        children: <Widget>[
-          Text("Friend"),
-          SearchTextField(
-            onChanged: (value) {
-              setState(() {
-                searchText = value;
-              });
-            },
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: friendList.where((name) => name.startsWith(searchText) || searchText == "").length,
-              itemBuilder: (context, index) {
-                String name = friendList.where((name) => name.startsWith(searchText) || searchText == "").elementAt(index);
-                return ListTile(
-                  leading: CircleAvatar(
-                    child: Icon(Icons.person),
-                  ),
-                  title: Text(name),
-                  // trailing: StingButton(otherUserName: "test", planId: 1),
-                );
+        child: Column(
+          children: <Widget>[
+            SearchTextField(
+              onChanged: (value) {
+                setState(() {
+                  searchText = value;
+                });
               },
             ),
-          ),
-        ],
-      ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: friendList
+                    .where((name) =>
+                        name.startsWith(searchText) || searchText == "")
+                    .length,
+                itemBuilder: (context, index) {
+                  String name = friendList
+                      .where((name) =>
+                          name.startsWith(searchText) || searchText == "")
+                      .elementAt(index);
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(Icons.person),
+                    ),
+                    title: Text(name),
+                    trailing: AddButton(
+                      friendName: name,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -57,13 +86,13 @@ class SearchTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: TextField(
         onChanged: onChanged,
         decoration: InputDecoration(
           hintText: "Search",
-          prefixIcon: Icon(Icons.search),
-          suffixIcon: Icon(Icons.clear),
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: const Icon(Icons.clear),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
             borderSide: BorderSide.none,
