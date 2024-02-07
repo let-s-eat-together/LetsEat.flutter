1eb6f45d2d73b26c42ed7eca6bd03b429a4988c2
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-02-08 05:47:51 +0900

    add: alertDialog

--- lets_eat/lib/screens/make_plan/qr_camera_screen.dart
+++ lets_eat/lib/screens/make_plan/qr_camera_screen.dart
@@ -29,18 +29,20 @@ class _QRCameraScreenState extends State<QRCameraScreen> {
       String? expirationDate, String? friendId, String token) async {
     try {
       final response = await http.post(
-        Uri.parse(baseUrl + ApiType.requestFriend.rawValue),
-        headers: API.getHeaderWithToken(token),
+        Uri.parse(baseUrl + ApiType.createPlan.rawValue),
+        // headers: API.getHeaderWithToken(token),
         body: jsonEncode(
           <String, dynamic>{
-            'expirationDate': expirationDate,
+            'expired_date': expirationDate,
             'userNumber': friendId,
           },
         ),
       );
-      debugPrint(response.body);
+      debugPrint('Success to request: ${response.body}');
+      _showAlertAndHandleResponse(response.statusCode, response.body);
     } catch (e) {
       debugPrint('Failed to request: $e');
+      _showAlertAndHandleResponse(500, '서버 오류');
     }
   }
 
@@ -59,12 +61,47 @@ class _QRCameraScreenState extends State<QRCameraScreen> {
           final List<Barcode> barcodes = capture.barcodes;
           final String rawString = barcodes[0].rawValue.toString();
           final List<String> datas = rawString.split('\n');
-          // for (final barcode in barcodes) {
-          //   debugPrint('${barcodes.length}: ${barcode.rawValue}');
-          // }
           parseData(datas, userProvider.user!.token);
         },
       ),
     );
+  }
+
+  void _showAlertAndHandleResponse(int statusCode, String body) async {
+    String message;
+    switch (statusCode) {
+      case 200:
+        message = '친구 요청을 성공적으로 보냈습니다.';
+        break;
+      case 400:
+        message = '잘못된 요청입니다.';
+        break;
+      case 401:
+        message = '토큰이 만료되었습니다. 다시 로그인해주세요.';
+        break;
+      case 500:
+        message = '서버 오류입니다. 잠시 후 다시 시도해주세요.';
+        break;
+      default:
+        message = '알 수 없는 오류가 발생했습니다.';
+    }
+
+    await showDialog<void>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('알림'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('확인'),
+          ),
+        ],
+      ),
+    );
+
+    // API 응답에 따른 추가 작업 (필요한 경우)
   }
 }
