743f890759163de89cd3e367632ab474c4c9cb1f
SongJungHyun <201802112@o.cnu.ac.kr>
2024-02-08 15:46:55 +0900

    feat: add toast msg

--- lets_eat/lib/utils/api_signup.dart
+++ lets_eat/lib/utils/api_signup.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:lets_eat/models/API.dart';
 import 'package:lets_eat/utils/api_login.dart';
@@ -17,15 +18,18 @@ Future<void> signupAPI(String? email, String? password, String? nickname,
         'username': nickname!,
       }),
     );
-    debugPrint('signup: ${response.body}');
-    debugPrint('회원가입 성공');
-    Navigator.pop(context);
-    loginAPI(email, password, context);
+    if (response.statusCode >= 200 && response.statusCode < 300){
+      debugPrint('signup: ${response.body}');
+      debugPrint('회원가입 성공');
+      Navigator.pop(context);
+      loginAPI(email, password, context);
+    } else {
+      // 서버에서 에러나는 원인을 알려줘야 함
+      // 이미 있는 유저입력 -> 이미 등록된 유저입니다.
+      // 서버오류 -> 일시적인 오류로 가입을 할 수 없습니다. \n잠시 후 다시 이용해 주세요.
+      Fluttertoast.showToast(msg: '회원가입 실패');
+    }
   } catch (e) {
-    print('회원가입 실패');
-    // 값이 틀려서 실패 or 다른 요인으로 실패 구분하기
-    print('~가 틀렸습니다');
-    print('잠시후 다시 시도해 주세요');
     debugPrint(e.toString());
     throw Exception('Failed to load data');
   }
